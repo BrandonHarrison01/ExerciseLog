a5314a13982c51e306b133396d2c6bd01e7cb960
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-11-22 22:43:13 -0700

    displaying routine object

--- components/RoutineFeed.js
+++ components/RoutineFeed.js
@@ -1,35 +1,43 @@
-import React, { useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
-import firestore from '@react-native-firebase/firestore'
-
-
-
-
+import firestore from '@react-native-firebase/firestore';
 
 function RoutineFeed() {
+  const [routines, setRoutines] = useState(null);
 
-    useEffect(() => {
-        firestore()
-            .collection('users1')
-            .doc('initial')
-            .collection('created-routines')
-            // .doc('bench-day')
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach(doc => {
-                    console.log(doc.data(), 'data')
-                })
-            })
-    }, [])
-    
+  useEffect(() => {
+    firestore()
+      .collection('users1')
+      .doc('initial')
+      .collection('created-routines')
+      .get()
+      .then((querySnapshot) => {
+        let r = [];
+        querySnapshot.forEach((doc) => {
+          r.push(doc.data());
+        });
+        setRoutines(r);
+      });
+  }, []);
+
+  const mapRoutines = () => {
     return(
-        <View>
-            <Text>
-                Feed Screen
-            </Text>
-        </View>
+        routines.map(routine => {
+            <TouchableOpacity>
+                <Text>test</Text>
+            </TouchableOpacity>
+        })
     )
+  }
+
+  return (
+    <View>
+        {
+            routines ? mapRoutines() : <Text>Waiting...</Text>
+        }
+    </View>
+  );
 }
 
-export default RoutineFeed
+export default RoutineFeed;
